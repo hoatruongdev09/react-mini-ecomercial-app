244bee3d2d8a7caecf4bfa25fab27f00b825a279
Hoa Truong <39894774+hoatruongdev09@users.noreply.github.com>
2022-08-14 22:32:18 +0700

    make category list page

---
+++ src/components/category-list/category-list.jsx
@@ -0,0 +1,19 @@
+import './category-list.styles.scss'
+import CategoryItem from '../category-item/category-item'
+import React from 'react'
+
+function CategoryList({categories}) {
+  return (
+    <div className="categories-container">
+      {
+        categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))
+      }
+
+
+    </div>
+  )
+}
+
+export default CategoryList
